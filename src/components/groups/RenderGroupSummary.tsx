79ec83b60e18f2800db646febf899e364367b8b6
Trygve Jørgensen <trygveabjo@gmail.com>
2024-12-27 00:33:40 +0100

    Major styling overhaul and updated overflow checks

--- src/components/groups/RenderGroupSummary.tsx
+++ src/components/groups/RenderGroupSummary.tsx
@@ -16,43 +16,62 @@ export function RenderGroupSummary({ group, description }: { group: Group, descr
     const [isOverflowing, setIsOverflowing] = useState(false);
     const descriptionRef = useRef<HTMLDivElement>(null);
 
+    // insert after first '>' in description
+    // WARNING: Very hacking solution as were modfying the very html and not using
+    // proper scripting to remove the first tag
+    const trimmedDescription = description?.slice(0, description.indexOf(">")) 
+        + ' class="mt-3" '
+        + description?.slice(description.indexOf(">"));
+    
+    // Check if the description is overflowing and set a shadow if it is the case
     useEffect(() => {
-        if (descriptionRef.current) {
-            setIsOverflowing(
-                descriptionRef.current.scrollHeight > descriptionRef.current.clientHeight
-            );
-        }
+        const checkOverflow = () => {
+            if (descriptionRef.current) {
+                setIsOverflowing(
+                    descriptionRef.current.scrollHeight > descriptionRef.current.clientHeight
+                );
+            }
+        };
+    
+        checkOverflow();
+        window.addEventListener('resize', checkOverflow);
+    
+        return () => {
+            window.removeEventListener('resize', checkOverflow);
+        };
     }, [description]);
-
+    
     return (
-        <div className="relative bg-zinc-100 dark:bg-zinc-800 rounded-3xl p-6 my-6 overflow-hidden shadow border border-transparent transition dark:hover:border-zinc-500">
-            <div className="flex flex-row content-center gap-4">
-                {group.image ? <img src={group.image} alt={group.name} className="size-9 rounded m-0" /> : <></>}
-                <h1 className="m-0 p-0">{group.name}</h1>
-            </div>
-
-            { group.leader && <div className="mt-2">Leder: <b>{group.leader.first_name} {group.leader.last_name}</b></div> }
-            <hr className="my-3" />
-
-            { group.description &&
-            ( <React.Fragment>
-                <div className="h-64 overflow-hidden relative bg-zinc-200 dark:bg-zinc-900 shadow-inner px-4"
-                    ref={descriptionRef} >
-                    {description ?
-                        <div dangerouslySetInnerHTML={{__html: description}} /> :
-                        <div>Ingen beskrivelse</div>
-                    }
-                    {isOverflowing && (
-                    <div className="absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t from-zinc-200 dark:from-zinc-900 to-transparent pointer-events-none" />
-                    )}
+        <div className="h-96 relative flex flex-col p-6 my-6 overflow-hidden bg-zinc-100 dark:bg-zinc-800 rounded-3xl shadow hover:shadow-lg border border-transparent transition dark:hover:border-zinc-500">
+            <div className="flex flex-row items-center gap-4">
+                {group.image ? <img src={group.image} alt={group.name} className="size-14 rounded m-0" /> : <></>}
+                
+                <div className="flex flex-col">
+                    <h1 className="m-0 p-0">{group.name}</h1>
+                    { group.leader && <div className="m-0">Leder: <b>{group.leader.first_name} {group.leader.last_name}</b></div> }
                 </div>
-                <br/>
-            </React.Fragment>
-            )}
+            </div>
+            
+            <hr className="mt-3 mb-0" />
+            
+            <div
+                className="grow overflow-hidden relative"
+                ref={descriptionRef}>
+                {description ?
+                    <div dangerouslySetInnerHTML={{__html: trimmedDescription}} className={"*:first-o:bg-amber-400"} /> :
+                    <p className="mt-3">Ingen beskrivelse</p>
+                }
+                {isOverflowing && (
+                    <div
+                        className="absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t from-zinc-100 dark:from-zinc-800 to-transparent pointer-events-none"/>
+                )}
+            </div>
+            
+            <hr className="mt-0 mb-3"/>
             
             <div className="flex flex-row content-center justify-between">
                 <Button target="_blank" href={`https://tihlde.org/grupper/${group.slug}`}>Les mer</Button>
-                <Button href={`mailto:${group.contact_email}`}>Kontakt {group.name}</Button>
+                <Button href={`mailto:${group.contact_email}`}>Kontakt gruppen</Button>
             </div>
         </div>
     );
